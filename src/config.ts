3b4ddad294184cd1fd3e2512ffc155619d6d8336
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-01-24 12:51:36 +0200

    creat eapp config file

---
+++ src/config.ts
@@ -0,0 +1,34 @@
+const CONFIG = {
+	months: [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December',
+	],
+	tags: [
+		'articles',
+		'css-in-js',
+		'browsers',
+		'frameworks',
+		'methodology',
+		'preprocessors',
+		'react',
+		'resets',
+		'standards',
+		'tools',
+		'websites',
+	],
+	years: Array(40)
+		.fill(0)
+		.map((_, i) => 1990 + i),
+};
+
+export default CONFIG;
